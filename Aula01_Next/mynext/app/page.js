2c87663be3a347ff5f1a69d69a996628735c0bf7
Guilherme Santos <guilherme.santos.sa@outlook.com>
2024-08-29 22:24:59 -0300

    Primeiro projeto em Next

---
+++ Aula01_Next/mynext/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 class="text-blue-700">Hello Wolrd !!!</h1>
+      <h2 class="text-red-600">O CORINTHIANS E MUITO RUUUIM</h2>
+      <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-indigo-500 hover:bg-sky-800">MEU DEUS</button>
+    </main>
+  );
+}
